df7489804aa97abbdc444d4ab9679d55f07a8c21
Jamie Bell <jamie.cs.bell@hotmail.com>
2023-05-17 15:04:39 +0100

    Updated user admin page and added tests

--- app/core/admin.py
+++ app/core/admin.py
@@ -3,6 +3,7 @@ from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext_lazy as _
 from core import models
 
+
 class UserAdmin(BaseUserAdmin):
     ordering = ['id']
     list_display = ['email', 'name']
@@ -35,7 +36,6 @@ class UserAdmin(BaseUserAdmin):
             )
         }),
     )
-
 
 
 admin.site.register(models.User, UserAdmin)
